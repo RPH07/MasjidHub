15ff3b9e59fe6755d32654e0de7b8411b89c6b55
RPH07 <renaldyputrahasli@gmail.com>
2025-05-05 20:20:07 +0700

    feat: Add Export, Kas, and Kegiatan components for admin pages

---
+++ masjid-frontend/src/pages/admin/Export.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Export extends Component {
+  render() {
+    return (
+      <div>Export</div>
+    )
+  }
+}
+
+export default Export
---
+++ masjid-frontend/src/pages/admin/Kegiatan.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Kegiatan extends Component {
+  render() {
+    return (
+      <div>Kegiatan</div>
+    )
+  }
+}
+
+export default Kegiatan
