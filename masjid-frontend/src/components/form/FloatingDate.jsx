fcf3b895f0900e8865dbdb293df39845176da8ba
RPH07 <renaldyputrahasli@gmail.com>
2025-05-28 02:18:27 +0700

    feat: Add FloatingDateInput component for date selection with floating label

---
+++ masjid-frontend/src/components/form/FloatingDate.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const FloatingDateInput = ({ label, name, value, onChange, required, icon }) => {
+  // Generate unique ID untuk input
+  const inputId = `floating-date-${name}`;
+  
+  return (
+    <div className="relative">
+      <input
+        id={inputId}
+        type="date"
+        name={name}
+        value={value}
+        onChange={onChange}
+        required={required}
+        className="peer w-full border rounded px-3 py-2 pl-10 focus:outline-none focus:ring-2 focus:ring-blue-500 "
+      />
+      <label
+        htmlFor={inputId}
+        className={`absolute left-10 ${value ? '-top-2.5 text-sm bg-white' : 'top-2.5 text-base bg-white'} px-1 text-gray-500 transition-all peer-focus:-top-2.5 peer-focus:text-sm peer-focus:text-blue-600 peer-focus:bg-white`}
+      >
+        {label}
+      </label>
+      <span className="absolute left-3 top-2.5 text-gray-500">
+        {icon}
+      </span>
+    </div>
+  );
+};
+
+export default FloatingDateInput;
