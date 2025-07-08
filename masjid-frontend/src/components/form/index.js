654973c47e6107cf8d7731d4b91fe027d3e22c5d
RPH07 <renaldyputrahasli@gmail.com>
2025-07-08 11:20:28 +0700

    feat: Re-add FloatingDate export and ensure FloatingSelect is included in form components

--- masjid-frontend/src/components/form/index.js
+++ masjid-frontend/src/components/form/index.js
@@ -1,3 +1,4 @@
 export {default as FloatingInput} from './FloatingInput';
 export {default as FloatingTextarea} from './FloatingTextarea';
 export {default as FloatingDate} from './FloatingDate';
+export {default as FloatingSelect} from './FloatingSelect';
