dd59730b04649c1ccfc67cdc9e9c8d2805585d0a
RPH07 <renaldyputrahasli@gmail.com>
2025-07-08 11:02:07 +0700

    feat: Add EditDonasi and ViewDonations components to the exports in index.js

--- masjid-frontend/src/components/donasi-components/components/index.js
+++ masjid-frontend/src/components/donasi-components/components/index.js
@@ -3,6 +3,8 @@ export { default as DaftarDonasi } from './DaftarDonasi'
 export { default as TambahDonasi } from './TambahDonasi'
 export { default as DonasiAktif } from './DonasiAktif'
 export { default as DonasiHistory } from './DonasiHistory'
+export { default as EditDonasi } from './EditDonasi'
+export {default as ViewDonations} from './ViewDonations'
 
 // Export shared components
 export * from './shared'
