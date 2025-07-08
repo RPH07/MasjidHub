09e22fc09041088063b7cf3a0600de2c0f44a5a0
RPH07 <renaldyputrahasli@gmail.com>
2025-07-08 11:22:36 +0700

    feat: Add index file for Kegiatan components to streamline exports

---
+++ masjid-frontend/src/components/kegiatan-components/components/index.js
@@ -0,0 +1,5 @@
+export { default as KegiatanForm } from './KegiatanForm';
+export { default as KegiatanList } from './KegiatanList';
+export { default as KegiatanCard } from './KegiatanCard';
+export { default as KategoriManager } from './KategoriManager';
+export * from './shared';
