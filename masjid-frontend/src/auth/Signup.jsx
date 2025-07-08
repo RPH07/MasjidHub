76d6dee3674e345227f4b042d23607c09c0005b7
RPH07 <renaldyputrahasli@gmail.com>
2025-07-08 11:00:47 +0700

    refactor: Remove commented-out API_BASE_URL import in Signup component

--- masjid-frontend/src/auth/Signup.jsx
+++ masjid-frontend/src/auth/Signup.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+// import {API_BASE_URL} from '../config/api';
 
 const RegisterPages = () => {
   const [formData, setFormData] = useState({
