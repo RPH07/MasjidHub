eced13c2db0dd30d51dfee2a887473dd852a3b8d
RPH07 <renaldyputrahasli@gmail.com>
2025-07-08 10:58:09 +0700

    feat: Add About and Contact pages to the router and enhance user layout with error boundary

--- masjid-frontend/src/App.jsx
+++ masjid-frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import About from './pages/About'; 
+import Contact from './pages/Contact'; 
 import LoginPages from './auth/Login';
 import ZakatForm from './pages/ZakatForm';
 import RegisterPages from './auth/Signup';
@@ -8,19 +10,31 @@ import AdminLayout from './components/layouts/AdminLayout';
 import Dashboard from './pages/admin/Dashboard';
 import KegiatanPage from './pages/admin/Kegiatan';
 import KasPage from './pages/admin/Kas.jsx';
-import DonasiPage from './pages/admin/Donasi'; // Import halaman donasi admin
+import DonasiPage from './pages/admin/Donasi';
 import AdminSignup from './auth/AdminSignup';
 import AdminRoute from './components/route-guard/AdminRoute';
 import ProtectedRoute from './components/route-guard/ProtectedRoute';
 import AuthCallback from './auth/AuthCallback';
 import UserDashboard from './pages/user/userDashoard';
-import Crowdfunding from './pages/user/Crowdfunding'
+import Crowdfunding from './pages/user/Crowdfunding';
+import UserLayout from './components/layouts/UserLayout'; 
+import KontribusiHistory from './pages/user/KontribusiHistory';
+import UserKegiatan from './pages/user/UserKegiatan';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
     errorElement: <div>Oops! Something went wrong. Please come back later.</div>,
+  },
+  {
+    path: "/about", 
+    element: <About />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
   },
   {
     path: "/login",
@@ -31,8 +45,8 @@ const router = createBrowserRouter([
     element: <RegisterPages />
   },
   {
-      path: "/admin/signup",
-      element: <AdminSignup />
+    path: "/admin/signup",
+    element: <AdminSignup />
   },
   {
     path: "/zakat",
@@ -43,44 +57,64 @@ const router = createBrowserRouter([
     element: <AuthCallback />,
   },
   {
+    path: "/crowdfunding",
+    element: <Crowdfunding />
+  },
+  {
     path: "/dashboard",
     element: (
       <ProtectedRoute>
-        <UserDashboard />
+        <UserLayout />
       </ProtectedRoute>
-    )
-  },
-  {
-    path: "/crowdfunding",
-    element: (
-      <ProtectedRoute>
-        <Crowdfunding />
-      </ProtectedRoute>
-    )
-  },
-  {
-    path: "/admin",
-    element:(
-    <AdminRoute>
-      <ProtectedRoute>
-        <AdminLayout />
-      </ProtectedRoute>
-    </AdminRoute>),
+    ),
     children: [
       {
-        index: true, // /admin
+        index: true,
+        element: <UserDashboard />
+      },
+      {
+        path: "zakat",
+        element: <ZakatForm />
+      },
+      {
+        path: "crowdfunding",
+        element: <Crowdfunding />
+      },
+      {
+        path: "kegiatan",
+        element: <UserKegiatan />
+      },
+      {
+        path: "kontribusi-history",
+        element: <KontribusiHistory />
+      }
+    ]
+  },
+  // Admin routes
+  {
+    path: "/admin",
+    element: (
+      <AdminRoute>
+        <ProtectedRoute>
+          <AdminLayout />
+        </ProtectedRoute>
+      </AdminRoute>
+    ),
+    children: [
+      {
+        index: true,
         element: <Dashboard />
       },
       {
-        path: "kegiatan", // /admin/kegiatan
+        path: "kegiatan",
         element: <KegiatanPage />
       },
       {
-        path: "kas", // /admin/kas
+        path: "kas",
         element: <KasPage />
       },
       {
-        path: "donasi", // /admin/donasi
+        path: "donasi",
         element: <DonasiPage />
       }
     ]
@@ -88,7 +122,12 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+  <ErrorBoundary>
+    <RouterProvider router={router} />
+  </ErrorBoundary>
+);
+
 }
 
 export default App;
