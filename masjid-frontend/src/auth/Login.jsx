bcb8adee33153be095e9ed8de169a7b4ae6642a5
RPH07 <renaldyputrahasli@gmail.com>
2025-07-08 11:00:31 +0700

    refactor: Remove unused API_BASE_URL import in Login component

--- masjid-frontend/src/auth/Login.jsx
+++ masjid-frontend/src/auth/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
 import { supabase } from '@/config/supabaseClient';
+// import {API_BASE_URL} from '../config/api'
 
 const LoginPages = () => {
     const navigate = useNavigate(); 
