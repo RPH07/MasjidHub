86c33696612d65b741c0c2f9186b61e8ec27229d
RPH07 <renaldyputrahasli@gmail.com>
2026-03-11 17:41:41 +0700

    feat: add upload functionality for Zakat with cloudinary integration

--- masjid-backend/routes/zakat.js
+++ masjid-backend/routes/zakat.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const zakatController = require('../models/controllers/zakatController');
 const {verifyToken} = require('../models/middleware/auth');
+const {upload} = require('../config/cloudinary')
 
 router.get('/', zakatController.getZakat);
 
@@ -12,5 +13,9 @@ router.post('/', zakatController.createZakat);
 
 // PUT
 router.put('/:id/validate', zakatController.verifyZakat);
+router.patch('/:id/upload', upload('zakat_bukti').single('bukti'), zakatController.uploadBuktiZakat);
+
+// DELETE
+router.delete('/:id', zakatController.deleteZakat)
 
 module.exports = router;
