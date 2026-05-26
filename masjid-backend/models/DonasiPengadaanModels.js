1ccb41346436d47f8e7767fa43b4e06e75405a39
RPH07 <renaldyputrahasli@gmail.com>
2026-05-26 16:19:57 +0700

    feat: add DonasiPengadaan model with associations to BarangPengadaan and User models

---
+++ masjid-backend/models/DonasiPengadaanModels.js
@@ -0,0 +1,100 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./../config/db');
+const user = require('./UserModels');
+const BarangPengadaan = require('./BarangPengadaanModels');
+
+const DonasiPengadaan = sequelize.define('DonasiPengadaan', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    barang_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    user_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    nama_donatur: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nominal: {
+        type: DataTypes.DECIMAL(15, 2),
+        allowNull: false
+    },
+    nominal_asli: {
+        type: DataTypes.DECIMAL(15, 2),
+        allowNull: false
+    },
+    metode_pembayaran: {
+        type: DataTypes.ENUM('transfer_bank', 'qris', 'tunai', 'cash'),
+        allowNull: false
+    },
+    bukti_transfer: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    status: {
+        type: DataTypes.ENUM('pending', 'approved', 'rejected'),
+        defaultValue: 'pending'
+    },
+    rejected_reason: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    validated_by: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+    validated_at: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    deleted_at: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    kontak_donatur: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }, 
+    catatan: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+    kode_unik: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    total_transfer: {
+        type: DataTypes.DECIMAL(12, 2),
+        allowNull: false
+    }
+}, {
+    tableName: 'donasi_pengadaan',
+    freezeTableName: true,
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+});
+
+BarangPengadaan.belongsTo(BarangPengadaan, {
+    foreignKey: 'barang_id',
+    as: 'barang'
+});
+
+BarangPengadaan.hasMany(DonasiPengadaan, {
+    foreignKey: 'barang_id',
+    as: 'donasi'
+});
+
+DonasiPengadaan.belongsTo(user, {
+    foreignKey: 'user_id',
+    as: 'user'
+});
+
+
+module.exports = DonasiPengadaan;
