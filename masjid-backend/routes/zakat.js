7f37ffcceb84d8c4cb9d82d1c79b0d089b1c9c6f
RPH07 <renaldyputrahasli@gmail.com>
2026-05-26 15:53:12 +0700

    refactor: enhance authentication middleware for zakat routes and clean up commented code

--- masjid-backend/routes/zakat.js
+++ masjid-backend/routes/zakat.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const zakatController = require('../models/controllers/zakatController');
-const {verifyToken} = require('../models/middleware/auth');
-const {upload} = require('../config/cloudinary')
+const { verifyToken, dkmOrAdmin, adminOnly } = require('../models/middleware/auth');
+const { upload } = require('../config/cloudinary');
 
-router.get('/', zakatController.getZakat);
+router.get('/', verifyToken, dkmOrAdmin, zakatController.getZakat);
 
-// POST
-// router.post('/generate-kode', zakatController.generateKodeUnik);
 router.post('/', zakatController.createZakat); 
-// router.post('/', verifyToken, zakatController.createZakat);
 
-// PUT
-router.put('/:id/validate', zakatController.verifyZakat);
 router.patch('/:id/upload', upload('zakat_bukti').single('bukti'), zakatController.uploadBuktiZakat);
 
-// DELETE
-router.delete('/:id', zakatController.deleteZakat)
+router.put('/:id/validate', verifyToken, dkmOrAdmin, zakatController.verifyZakat);
+
+router.delete('/:id', verifyToken, adminOnly, zakatController.deleteZakat);
 
 module.exports = router;
