f95965b64aac6a21152e71cfdbe4d84cb53d0193
RPH07 <renaldyputrahasli@gmail.com>
2026-05-30 01:14:41 +0700

    feat: add pengadaan routes for creating and updating barangPengadaan

---
+++ masjid-backend/routes/pengadaan.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const barangPengadaanController = require('../models/controllers/barangPengadaanController');
+const { verifyToken, dkmOrAdmin } = require('../models/middleware/auth');
+const { upload } = require('../config/cloudinary');
+
+router.post(
+    '/',
+    verifyToken,
+    dkmOrAdmin,
+    upload('donasi-program').single('foto_barang'),
+    barangPengadaanController.createBarangPengadaan
+);
+
+router.put(
+    '/:id',
+    verifyToken,
+    dkmOrAdmin,
+    upload('donasi-program').single('foto_barang'),
+    barangPengadaanController.updateProgramPengadaan
+);
+
+module.exports = router;
