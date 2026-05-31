788a35ec3ed8e38976bef8f1d0f68a5116bbb545
RPH07 <renaldyputrahasli@gmail.com>
2026-05-31 12:05:51 +0700

    feat: add route for creating donation with file upload in pengadaan

--- masjid-backend/routes/pengadaan.js
+++ masjid-backend/routes/pengadaan.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const barangPengadaanController = require('../models/controllers/barangPengadaanController');
+const donasiPengadaanController = require('../models/controllers/donasiPengadaanController');
 const { verifyToken, dkmOrAdmin } = require('../models/middleware/auth');
 const { upload } = require('../config/cloudinary');
 
@@ -20,6 +21,13 @@ router.post(
     dkmOrAdmin,
     upload('donasi-program').single('foto_barang'),
     barangPengadaanController.createBarangPengadaan
+);
+
+router.post(
+    '/donasi',
+    verifyToken,
+    upload('donasi-program').single('bukti_transfer'),
+    donasiPengadaanController.createDonasiPengadaan
 );
 
 router.put(
