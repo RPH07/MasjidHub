642622963808b169edfa657594bf57c9501c37d7
RPH07 <renaldyputrahasli@gmail.com>
2026-06-11 09:57:50 +0700

    feat: remove adminOnly middleware from Zakat routes

--- masjid-backend/routes/zakat.js
+++ masjid-backend/routes/zakat.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const zakatController = require('../models/controllers/zakatController');
-const { verifyToken, dkmOrAdmin, adminOnly } = require('../models/middleware/auth');
+const { verifyToken, dkmOrAdmin } = require('../models/middleware/auth');
 const { upload } = require('../config/cloudinary');
 
 router.get('/', verifyToken, dkmOrAdmin, zakatController.getZakat);
@@ -11,7 +11,5 @@ router.post('/', zakatController.createZakat);
 router.patch('/:id/upload', upload('zakat_bukti').single('bukti'), zakatController.uploadBuktiZakat);
 
 router.put('/:id/validate', verifyToken, dkmOrAdmin, zakatController.verifyZakat);
-
-router.delete('/:id', verifyToken, adminOnly, zakatController.deleteZakat);
 
 module.exports = router;
