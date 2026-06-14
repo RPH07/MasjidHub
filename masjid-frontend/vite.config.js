7c454b113bcf8349d0ab658dadd293575c0b5f01
RPH07 <renaldyputrahasli@gmail.com>
2026-06-14 11:33:32 +0700

    fix: update alias resolution in vite.config.js to use fileURLToPath for consistency

--- masjid-frontend/vite.config.js
+++ masjid-frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 
 // https://vite.dev/config/
@@ -9,8 +9,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
