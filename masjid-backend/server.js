faa52f45f430fb630028120c574f51b804951b9d
RPH07 <renaldyputrahasli@gmail.com>
2026-07-15 18:27:18 +0700

    feat: update CORS configuration to include additional origins

--- masjid-backend/server.js
+++ masjid-backend/server.js
@@ -20,7 +20,9 @@ app.use(express.static('public'));
 
 app.use(cors({
   origin: [
+    'masjidnurulilmi.my.id',
     'http://localhost:5173',
+    'http://localhost:5174',
     'http://localhost:3000',  
     'http://localhost:3001'  
   ], 
