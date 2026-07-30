8e5d9321dc9c4c1932b1ef0dbd6199d213cd05aa
RPH07 <renaldyputrahasli@gmail.com>
2026-07-30 11:23:04 +0700

    feat: create index file for Donasi feature to export main and shared components

---
+++ masjid-frontend/src/features/donasi/components/index.js
@@ -0,0 +1,10 @@
+// Export semua komponen utama
+export { default as DaftarDonasi } from './DaftarDonasi'
+export { default as TambahDonasi } from './TambahDonasi'
+export { default as DonasiAktif } from './DonasiAktif'
+export { default as DonasiHistory } from './DonasiHistory'
+export { default as EditDonasi } from './EditDonasi'
+export {default as ViewDonations} from './ViewDonations'
+
+// Export shared components
+export * from './shared'
