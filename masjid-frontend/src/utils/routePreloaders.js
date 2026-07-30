8ceac1901034ee2dfcc88304870bb94fe1dd847d
RPH07 <renaldyputrahasli@gmail.com>
2026-07-30 13:50:01 +0700

    fix: correct import paths for user and admin dashboard pages in routePreloaders

--- masjid-frontend/src/utils/routePreloaders.js
+++ masjid-frontend/src/utils/routePreloaders.js
@@ -23,23 +23,23 @@ const preload = (imports) => {
 
 export const preloadUserDashboardPages = () => {
   preload([
-    () => import('../pages/user/userDashoard'),
-    () => import('../pages/user/KontribusiHistory'),
-    () => import('../pages/user/UserKegiatan'),
-    () => import('../pages/ZakatForm'),
-    () => import('../pages/user/Crowdfunding')
+    () => import('@/pages/user/UserDashboard'),
+    () => import('@/pages/user/KontribusiHistory'),
+    () => import('@/pages/user/UserKegiatan'),
+    () => import('@/pages/ZakatForm'),
+    () => import('@/pages/user/Crowdfunding')
   ]);
 };
 
 export const preloadAdminDashboardPages = () => {
   preload([
-    () => import('../pages/admin/Dashboard'),
-    () => import('../pages/admin/Kegiatan'),
-    () => import('../pages/admin/Kas.jsx'),
-    () => import('../pages/admin/Donasi'),
-    () => import('../pages/admin/UserAccess'),
-    () => import('../pages/admin/VerifikasiTransaksi'),
-    () => import('../pages/admin/TransparansiAdmin'),
-    () => import('../pages/admin/ZakatSettings')
+    () => import('@/pages/admin/Dashboard'),
+    () => import('@/pages/admin/Kegiatan'),
+    () => import('@/pages/admin/Kas.jsx'),
+    () => import('@/pages/admin/Donasi'),
+    () => import('@/pages/admin/UserAccess'),
+    () => import('@/pages/admin/VerifikasiTransaksi'),
+    () => import('@/pages/admin/TransparansiAdmin'),
+    () => import('@/pages/admin/ZakatSettings')
   ]);
 };
