31cd2e444d673fe1ead09ce6c3534af81160f261
RPH07 <renaldyputrahasli@gmail.com>
2026-08-06 00:44:23 +0700

    feat: re-export loading components for improved modularity

--- masjid-frontend/src/features/activities/components/index.js
+++ masjid-frontend/src/features/activities/components/index.js
@@ -3,3 +3,4 @@ export { default as ActivityList } from './ActivityList';
 export { default as ActivityCard } from './ActivityCard';
 export { default as CategoryManager } from './CategoryManager';
 export * from './shared';
+export * from './loading';
